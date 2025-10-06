47657eda729252e5779af541607d5967b1ba0aaf
Wilona2 <2172985@cegepoutaouais.qc.ca>
2025-10-06 13:58:24 -0400

    Fichier Maryam

---
+++ maryam.py
@@ -0,0 +1,15 @@
+#2. Fonction qui reçoit une liste de legos et une couleur et qui retourne le pourcentage de blocs de cette couleur
+# a. Programmer la fonction
+
+def liste_legos(couleurs, pourcentages_blocs) -> int:
+    """
+    la fonctions qui recois la liste de legos et sa couleurs avec son pourcenatge de la couleur
+    :param couleurs: La couleur assigner au bloc de couleur
+    :param pourcentages_blocs: Le pourcentage de couleurs dans un bloc de lego
+    :return: le bloc de lego obtenu
+    """
+    couleurs + pourcentages_blocs
+    return
+###lalala
+
+
