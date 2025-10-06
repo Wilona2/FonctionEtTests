7ddcc58b3a1378094a8b52b1d60d3799826fb7e8
Wilona2 <2172985@cegepoutaouais.qc.ca>
2025-10-06 13:52:31 -0400

    Fonction 1

--- fctListes.py
+++ fctListes.py
@@ -22,7 +22,41 @@ qui retourne il y a combien de blocs de chaque couleur en moyenne
 # Répartition des tâches :
 """                     
 a   b   c
-1   2   3   Nom :                   
-2   3   1   Nom :                   
-3   1   2   Nom :                   
+1   2   3   Nom : Wilona a1 -> fait(??)               
+2   3   1   Nom : Maryam                
+3   1   2   Nom : Matis               
 """
+
+def points(pts_j1: list[int], pts_j2: int) -> int:
+    """
+    Fonction qui reçoit les points de vies et les points de défenses d'un joueur et
+    les points d'attaques de l'autre et qui retourne les points de vies restants après l'attaque.
+    :param pts_j1: liste contenant les points de vies et les points de défenses du joueur 1 (ex: pts_j1[pts_vies, pts_def])
+    :param pts_j2: points d'attaques du joueur 2
+    :return: points de vies restants au premier joueur après l'attaque.
+    """
+    pts_dmg_j2 = pts_j2 - pts_j1[1]
+    pts_restants_j1 = pts_j1[0] - pts_dmg_j2
+    return pts_restants_j1
+
+    #
+
+
+
+    #(3(a)Matis
+    def moyenne_legos(legos: liste) :
+        # compteurs couleur
+for bloc in legos:
+    if bloc == "rouge":
+        rouge += 1
+    elif bloc == "bleu":
+        bleu += 1
+    elif bloc == "vert":
+        vert += 1
+    elif bloc -- "jaune":
+        jaune += 1
+
+        # afficher moyenne
+        print("Rouge :", nb_rouge)
+        print("Bleu:", nb_bleu)
+        print("Vert:", nb_vert)
